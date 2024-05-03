46fef1d271020aafb6060a532e9a94f7b6b57888
Ludvig <ludvig.fendert@appliedtechnology.se>
2024-05-03 10:33:55 +0200

    Added Stopwatch class and imports

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+//An instance of our Stopwatch class(where the code will be)
+        Stopwatch stopwatch = new Stopwatch();
+
+
+
+
     }
 }
---
+++ src/Stopwatch.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+public class Stopwatch implements ActionListener {
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+
+    }
+}
