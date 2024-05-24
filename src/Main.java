01299c61ca9b85ccf87c7eef697d62d1f0452604
Ludvig <ludvig.fendert@appliedtechnology.se>
2024-05-24 16:22:01 +0200

    comment fixed

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-//An instance of our Stopwatch class(where the code will be)
         Stopwatch stopwatch = new Stopwatch();
         //just for fun
-       /* Stopwatch stopwatch1 = new Stopwatch();
+    /*    Stopwatch stopwatch1 = new Stopwatch();
         Stopwatch stopwatch2 = new Stopwatch();
         Stopwatch stopwatch3 = new Stopwatch();
 */
